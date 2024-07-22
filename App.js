d0dd33e1ff1e98e009e1e9abc4fb46ed32bb94f5
vj-logan <vsr.24252@gmail.com>
2024-07-22 01:24:58 +0100

    inception

---
+++ App.js
@@ -0,0 +1,3 @@
+const heading = React.createElement("div", {id: "Heading"}, React.createElement("h1", {}, "This is Heading") );
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
